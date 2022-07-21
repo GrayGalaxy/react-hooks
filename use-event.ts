ff1ef9575ff01959eacc2890232364c5f4f76585
Avirup Ghosh <ag.avirup1997@gmail.com>
2022-07-22 01:29:58 +0530

    updated use-event

--- use-event.ts
+++ use-event.ts
@@ -1,36 +1,62 @@
 import { useEffect, useRef } from 'react'
-// type definitions
+
 import type { RefObject } from 'react'
+
+// Windows Event based interface
+export function useEvent<
+	EW extends keyof WindowEventMap
+>(
+	event_name: EW,
+	callback: (event: WindowEventMap[EW]) => void,
+	element?: undefined,
+): void
+// HTML Elementh Based interface
+export function useEvent<
+	EH extends keyof HTMLElementEventMap,
+	T extends HTMLElement = HTMLDivElement,
+>(event_name: EH, handler: (event: HTMLElementEventMap[EH]) => void, element: RefObject<T>): void
+// Document Event based interface
+export function useEvent<
+	ED extends keyof DocumentEventMap
+>(
+	event_name: ED,
+	callback: (event: DocumentEventMap[ED]) => void,
+	element: RefObject<Document>,
+): void
 
 /**
  * Hook to use event listener
- * @param key - Window event name
+ * @param event_name - Window or Element event name
  * @param callback - Callback function
  * @param element - Element to attach event listener to
  */
-export function useEvent<T extends HTMLElement = HTMLDivElement>(
-	event: keyof WindowEventMap,
-	callback: (event: Event) => void,
+export function useEvent<
+	EW extends keyof WindowEventMap,
+	EH extends keyof HTMLElementEventMap,
+	T extends HTMLElement = HTMLDivElement,
+>(
+	event_name: EW,
+	callback: (event: WindowEventMap[EW] | HTMLElementEventMap[EH] | Event) => void,
 	element?: RefObject<T>,
 ) {
 	// Create a ref that stores handler
-	const handler = useRef<(event: Event) => void>()
+	const handler = useRef(callback)
 
 	useEffect(() => {
 		// Define the listening target
 		const targetElement: T | Window = element?.current || window
-		if (!(targetElement && targetElement.addEventListener)) return
-		// Update saved handler if necessary
-		if (handler.current !== callback) handler.current = callback
+		if (!(targetElement && targetElement.addEventListener)) {
+			return
+		}
 		// Add event listener
-		const eventListener = (event: Event) => {
+		const eventListener: typeof callback = (event) => {
 			if (!!handler?.current) handler.current(event)
 		}
-		targetElement.addEventListener(event, eventListener)
+		targetElement.addEventListener(event_name, eventListener)
 		return () => {
-			targetElement.removeEventListener(event, eventListener)
+			targetElement.removeEventListener(event_name, eventListener)
 		}
-	}, [event, element, callback])
+	}, [event_name, element, callback])
 }
 
 export default useEvent
